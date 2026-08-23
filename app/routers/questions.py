961e842b62ea5b3f37d48b411e1071bb1e178091
Umaima Mirza <assistance.healthinsight@gmail.com>
2026-08-23 09:39:49 +0530

    move questions into router

--- app/routers/questions.py
+++ app/routers/questions.py
@@ -4,6 +4,30 @@ router = APIRouter()
 
 @router.get("/questions")
 def questions():
-    return {
-        "debug": "THIS IS THE NEW FILE"
-    }
+    return [
+        {
+            "id": 1,
+            "question": "What is your age?",
+            "type": "number"
+        },
+        {
+            "id": 2,
+            "question": "Do you smoke?",
+            "type": "boolean"
+        },
+        {
+            "id": 3,
+            "question": "Do you have diabetes?",
+            "type": "boolean"
+        },
+        {
+            "id": 4,
+            "question": "Do you exercise regularly?",
+            "type": "boolean"
+        },
+        {
+            "id": 5,
+            "question": "Gender",
+            "type": "text"
+        }
+    ]
