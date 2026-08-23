dd8d0c88a4c19187da353f6e94f2ff0be015efe0
Umaima Mirza <assistance.healthinsight@gmail.com>
2026-08-23 07:32:43 +0530

    Add expanded health survey questions

--- app/routers/questions.py
+++ app/routers/questions.py
@@ -31,5 +31,30 @@ def questions():
             "id": 5,
             "question": "Gender",
             "type": "text"
+        },
+        {
+            "id": 6,
+            "question": "Do you have high blood pressure?",
+            "type": "boolean"
+        },
+        {
+            "id": 7,
+            "question": "Does your household have health insurance coverage?",
+            "type": "boolean"
+        },
+        {
+            "id": 8,
+            "question": "How many hours do you sleep per night?",
+            "type": "number"
+        },
+        {
+            "id": 9,
+            "question": "How many people live in your household?",
+            "type": "number"
+        },
+        {
+            "id": 10,
+            "question": "Did anyone in your household need medical care but not receive it in the last 12 months?",
+            "type": "boolean"
         }
     ]
